Normalization factors fixed Material AR eff implemented

pyyellin/_limit.py
@@ -229,12 +229,15 @@ class Limit:
         :return: Corresponding cdf values to the energy values in experimental data.
         :rtype: list
         """
-        corresponding_cdf_values_to_energy = []
-        for energy_value in self.data:
-            for i in range(len(cdf[0])):
-                if cdf[0][i] - cdf[0][0] > energy_value:
-                    corresponding_cdf_values_to_energy.append(cdf[1][i-1])
-                    break
+        new_x = sorted(list(self.data)+list(cdf[0]))
+        y_interp = np.interp(new_x, cdf[0], cdf[1])
+        corresponding_cdf_values_to_energy = y_interp[np.intersect1d(new_x, self.data, return_indices=True)[1]]
+        # corresponding_cdf_values_to_energy = []
+        # for energy_value in self.data:
+        #     for i in range(len(cdf[0])):
+        #         if cdf[0][i] - cdf[0][0] > energy_value:
+        #             corresponding_cdf_values_to_energy.append(cdf[1][i-1])
+        #             break
         corresponding_cdf_values_to_energy = list(np.array(corresponding_cdf_values_to_energy)-min(corresponding_cdf_values_to_energy))
         corresponding_cdf_values_to_energy = list(np.array(corresponding_cdf_values_to_energy)/max(corresponding_cdf_values_to_energy))
         return corresponding_cdf_values_to_energy

pyyellin/_signal_models.py
@@ -6,6 +6,8 @@ from scipy.special import erf
 from scipy.stats import norm
 from scipy import optimize
 
+from matplotlib import pyplot as plt
+
 
 class SignalModel:
     """
@@ -35,10 +37,13 @@ class SignalModel:
         self.resolution = 0.
         self.threshold = 0.
         self.upper_integral_limit = 0.
+        self.energy_grid_step_size = 0.0005
         self.materials = []
-        self.normalization_factor = 1.
+        self.pdf_sum_normalization_factor = 1.
+        self.pdf_sum_convoluted_normalization_factor = 1.
         self.normalization_list = []
         self.cut_eff = []
+        self.material_AR_eff = []
         self.energy_grid = []
 
     def set_detector(self, exposure: float, resolution: float, threshold: float, materials: list,
@@ -60,8 +65,11 @@ class SignalModel:
         self.threshold = threshold
         self.materials = materials
         self.upper_integral_limit = upper_integral_limit
-        self.energy_grid = np.arange(threshold-3*resolution, upper_integral_limit+3*resolution, energy_grid_step_size)
+        self.energy_grid_step_size = energy_grid_step_size
+        # self.energy_grid = np.arange(threshold-3*resolution, upper_integral_limit+3*resolution, energy_grid_step_size)
+        self._set_material_AR_eff()
         return
+
 
     def pdf(self, pars: float):
         """
@@ -73,6 +81,28 @@ class SignalModel:
         """
         rates_per_energy_list = []
         self.normalization_list = []
+
+        energies_x_min_3 = []
+        for element in self.materials:
+            energy_grid_mock = np.arange(self.threshold-3*self.resolution, 1000, 0.05)
+            A = element[0]
+            M_N = A*self.M_P
+            m_chi = np.copy(pars)*1e6
+            mu_N = M_N*m_chi/(M_N+m_chi)
+            v_min = np.sqrt(energy_grid_mock * M_N / (2 * mu_N ** 2))
+            x_min = np.sqrt(3*v_min**2/(2*self.W**2))
+            z = np.sqrt(3/2)*self.V_ESC/self.W
+            eta = np.sqrt(3/2)*self.V_EARTH/self.W
+            x_min_3_mock_list = [item for item in x_min if item >= (z+eta)]
+            energies_x_min_3.append(energy_grid_mock[len(energy_grid_mock)-len(x_min_3_mock_list)])
+        print(f'm_chi: {np.copy(pars)} GeV')
+        print('max(energies_x_min_3):', max(energies_x_min_3))
+        if max(energies_x_min_3) < 1:
+            max_energy = 1.
+        else:
+            max_energy = max(energies_x_min_3)
+        self.energy_grid = np.arange(self.threshold-3*self.resolution, max_energy, self.energy_grid_step_size)
+
         for element in self.materials:
             A = element[0]
             M_N = A*self.M_P  # nucleon mass [keV/c^2]
@@ -105,7 +135,8 @@ class SignalModel:
             integral *= 1/(n*eta)*np.sqrt(3/(2*np.pi*self.W**2))
             rates_per_energy = self.RHO_X/(2.*mu_p**2*m_chi)*A**2*f**2*integral  # total interaction rate R per energy E [c^2/(hbar*keV)] per sigma [pbarn]
             rates_per_energy *= self.DIMENSION_FACTOR
-            rates_per_energy = rates_per_energy*self.cut_eff
+            rates_per_energy = rates_per_energy*self.cut_eff[:len(rates_per_energy)]
+            rates_per_energy = rates_per_energy*self.material_AR_eff[list(self.materials).index(element)][:len(rates_per_energy)]
             normalization = integrate.trapz(rates_per_energy, self.energy_grid)  # normalization factor of the pdf
             rates_per_energy = rates_per_energy/normalization  # normalized pdf
             rates_per_energy_list.append(rates_per_energy)
@@ -219,8 +250,7 @@ class SignalModel:
                     break
         return np.array(sample)
 
-    @staticmethod
-    def pdf_sum(pdf: list, materials: list):
+    def pdf_sum(self, pdf: list, materials: list):
         """
         Sum the pdfs of the given materials according to their percentage in the molecule.
 
@@ -232,7 +262,9 @@ class SignalModel:
 
         pdf_sum = np.zeros(np.shape(pdf[0])[0])
         for i in range(len(pdf[1])):
-            pdf_sum += pdf[1][i]*materials[i][1]
+            pdf_sum += pdf[1][i]*materials[i][1]*self.normalization_list[i]
+        self.pdf_sum_normalization_factor = integrate.trapz(pdf_sum, self.energy_grid)  # normalization factor of the pdf
+        pdf_sum = pdf_sum/self.pdf_sum_normalization_factor  # normalized pdf
 
         return pdf[0], pdf_sum
 
@@ -262,11 +294,11 @@ class SignalModel:
         :return: Expected amount of events μ.
         :rtype: numpy array
         """
-        pdf_sum = pdf_sum_convoluted[1]*self.cut_eff
-        self.normalization_factor = sum([self.normalization_list[i]*self.materials[i][1] for i in range(len(self.materials))])
-        pdf_sum_limited_x = pdf_sum_convoluted[0][np.where(pdf_sum_convoluted[0] > self.threshold)[0][0]:np.where(pdf_sum_convoluted[0] < self.upper_integral_limit)[0][-1]+1]
-        pdf_sum_limited_y = pdf_sum[np.where(pdf_sum_convoluted[0] > self.threshold)[0][0]:np.where(pdf_sum_convoluted[0] < self.upper_integral_limit)[0][-1]+1]
-        mus = integrate.trapz(pdf_sum_limited_y, pdf_sum_limited_x)*np.array(sigmas)*self.normalization_factor*self.exposure
+        pdf_sum = pdf_sum_convoluted[1]*self.cut_eff[:len(pdf_sum_convoluted[1])]
+        pdf_sum_convoluted_limited_x = pdf_sum_convoluted[0][np.where(pdf_sum_convoluted[0] > self.threshold)[0][0]:np.where(pdf_sum_convoluted[0] < self.upper_integral_limit)[0][-1]+1]
+        pdf_sum_convoluted_limited_y = pdf_sum[np.where(pdf_sum_convoluted[0] > self.threshold)[0][0]:np.where(pdf_sum_convoluted[0] < self.upper_integral_limit)[0][-1]+1]
+        mus = integrate.trapz(pdf_sum_convoluted_limited_y, pdf_sum_convoluted_limited_x)*np.array(sigmas)*self.pdf_sum_convoluted_normalization_factor*self.exposure
+        print('mus:', mus)
         return mus
 
     def set_cut_eff(self, file_name):
@@ -284,7 +316,8 @@ class SignalModel:
             dataset = [line.split('\t') for line in dataset if line[0] != '#']
         self.cut_eff.append([float(number[0]) for number in dataset])
         self.cut_eff.append([float(number[1]) for number in dataset])
-        y_interp = np.interp(self.energy_grid, self.cut_eff[0], self.cut_eff[1])
+        energy_grid = np.arange(self.threshold-3*self.resolution, 1000, self.energy_grid_step_size)
+        y_interp = np.interp(energy_grid, self.cut_eff[0], self.cut_eff[1])
         self.cut_eff = np.copy(y_interp)
         return
 
@@ -298,8 +331,35 @@ class SignalModel:
         """
         gauss_x = np.arange(-10*self.resolution, 10*self.resolution, 0.0005)  # 10 sigma Bereich, 0.5 eV steps
         gauss = norm.pdf(gauss_x, scale=self.resolution)
-        pdf_sum_convoluted = np.convolve(pdf_sum[1], gauss, mode='same')/np.sum(gauss)
+        pdf_sum_convoluted = np.convolve(pdf_sum[1]*self.pdf_sum_normalization_factor, gauss, mode='same')/np.sum(gauss)
+        self.pdf_sum_convoluted_normalization_factor = integrate.trapz(pdf_sum[1], self.energy_grid)  # normalization factor of the pdf
+        pdf_sum_convoluted = pdf_sum_convoluted/self.pdf_sum_convoluted_normalization_factor  # normalized pdf
         return self.energy_grid, pdf_sum_convoluted
+
+    def _set_material_AR_eff(self):
+        """
+        Set probability of being in the acceptance region for each material.
+
+        :return: None
+        """
+        for i in range(len(self.materials)):
+            y_interp = []
+            x_axis = []
+            y_axis = []
+            with open(self.materials[i][-1], 'r', encoding='UTF8', newline='') as f:
+                dataset = f.readlines()
+                dataset = [line.strip('\n') for line in dataset if line[0] != '#']
+                dataset = [line.split('\t') for line in dataset if line[0] != '#']
+            x_axis.append([float(number[0]) for number in dataset])
+            y_axis.append([float(number[1]) for number in dataset])
+            energy_grid = np.arange(self.threshold-3*self.resolution, 1000, self.energy_grid_step_size)
+            y_interp = np.interp(energy_grid, x_axis[0], y_axis[0])
+            plt.plot(energy_grid, y_interp)
+            plt.title(f'{self.materials[i][2]}')
+            plt.xlim([0, 30])
+            plt.show()
+            self.material_AR_eff.append(y_interp)
+        return
 
     # m_N_min is the nucleus with the smallest mass (for NaI its Na)  # TODO: Include?
     def _e_max_func(self, m_dm, materials):

tests/test_our_model.py
@@ -3,8 +3,6 @@ import pyyellin as yell
 import timeit
 from matplotlib import pyplot as plt
 
-
-
 with open('C3P1_DetA_DataRelease_SI.xy', 'r', encoding='UTF8', newline='') as f:
     dataset = f.readlines()
     dataset = [line.strip('\n') for line in dataset if line[0] != '#']
@@ -19,26 +17,28 @@ x_axis_plot = [float(number.split()[0]) for number in dataset]
 # plt.show()
 
 test_list = [np.round(x*10**y, 3) for y in range(-2, 2) for x in range(1, 10)]
-print(test_list)
+# print(test_list)
 
 start = timeit.default_timer()
 
-m_chi = np.geomspace(1., 100., 25)
+m_chi = np.geomspace(1., 100., 5)
 m_chi = list(m_chi)
 for mass in x_axis_plot:
     m_chi.append(mass)
+m_chi.append(10.)
 m_chi.sort()
 m_chi = list(dict.fromkeys(m_chi))
 m_chi = np.array(m_chi)
-print(m_chi)
-# m_chi = [10.]
-# m_chi = np.linspace(3.5, 20, 10)
+# print(m_chi)
 sample_size = 10000
+materials = [[40.078, 40.078/287.914, 'Ca', 'C3P1_DetA_eff_AR_Ca.dat'],
+             [183.84, 183.84/287.914, 'W', 'C3P1_DetA_eff_AR_W.dat'],
+             [15.999, 15.999*4/287.914, 'O', 'C3P1_DetA_eff_AR_O.dat']]
 # materials = [[23., 23/150, 'Na'], [127., 127/150, 'I']]
-materials = [[40.078, 40.078/287.914, 'Ca'], [183.84, 183.84/287.914, 'W'], [15.999, 15.999*4/287.914, 'O']]
+# m_chi = [10.]
 
 omega = yell.Ilok()
-omega.set_detector(5.689, 0.0046, 0.0301, materials, 16.)
+omega.set_detector(5.594, 0.0046, 0.0301, materials, 16.)
 omega.set_cut_eff('C3P1_DetA_cuteff.dat')
 omega.set_masses(m_chi)
 # omega.set_sampling_size(sample_size)  # Optional, only needed for rvs functions
@@ -51,7 +51,51 @@ omega.set_confidence_level(0.9)
 omega.get_data('C3P1_DetA_AR.dat')
 omega.set_table_variables(False, 100, 'table')
 omega.get_limit()
-
+#
+# print(i_want_list)
+# print(i_want_list[list(m_chi).index(10.)])
+#
+# with open('C:/Users/Fatih/Desktop/Project Minerva/Projektarbeit 1/xy_NaI.txt', 'r') as f:
+#     lines = f.readlines()[1:]
+#     line = [i.split() for i in lines]
+#     f.close()
+# energy = [float(i[0]) for i in line]
+# na = [float(i[1]) for i in line]
+# iod = [float(i[2]) for i in line]
+# nai = [float(i[3]) for i in line]
+#
+# plt.yscale("log")
+# # plt.xlim(0, 40)
+# # plt.ylim(10**(-12), 10**0)
+# plt.plot(energy, na, label='na')
+# plt.plot(energy, iod, label='iod')
+# plt.plot(energy, nai, label='nai')
+# for material_index in range(len(materials)):
+#     plt.plot(i_want_list[list(m_chi).index(10.)][0][0], i_want_list[list(m_chi).index(10.)][0][1][material_index],
+#              label=f'pdf_{materials[material_index]}')
+# plt.legend()
+# plt.show()
+#
+# plt.yscale('log')
+# # plt.xlim(0, 40)
+# # plt.ylim(10**(-12), 10**0)
+# plt.plot(energy, na, label='na')
+# plt.plot(energy, iod, label='iod')
+# plt.plot(energy, nai, label='nai')
+# plt.plot(i_want_list[list(m_chi).index(10.)][3][0], i_want_list[list(m_chi).index(10.)][3][1], label='pdf_sum')
+# plt.legend()
+# plt.show()
+#
+# plt.yscale("log")
+# # plt.xlim(0, 40)
+# # plt.ylim(10**(-12), 10**0)
+# plt.plot(energy, na, label='na')
+# plt.plot(energy, iod, label='iod')
+# plt.plot(energy, nai, label='nai')
+# plt.plot(i_want_list[list(m_chi).index(10.)][6][0], i_want_list[list(m_chi).index(10.)][6][1], label='pdf_sum_convoluted')
+# plt.legend()
+# plt.show()
+#
 stop = timeit.default_timer()
 print('Time: ', stop - start)
 